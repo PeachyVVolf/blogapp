584b02cb76e051cb9722202f73b05c40799b0b34
Ahmad Ajmal <ahmadajmal9301@gmail.com>
2022-05-22 22:32:41 +0500

    Small Change

--- src/components/singlePost/SinglePost.js
+++ src/components/singlePost/SinglePost.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './SinglePost.css';
-import singlepostImg from '../../images/'
+import singlepostImg from '../../images/postImg.jpg';
 
 const SinglePost = () => {
     return (
         <div className='singlepost'>
             <div className='singlepostWrapper'>
-                <img className='singlepostImg' />
+                <img className='singlepostImg' src={singlepostImg} alt='postImg' />
             </div>
         </div>
     )
